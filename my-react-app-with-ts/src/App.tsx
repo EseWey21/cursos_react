c52b0ef0ab875707cd1a68c087e994840f8db6c3
SajitVentura <elopezv1700@alumno.ipn.mx>
2025-06-20 08:52:36 -0600

    Tipado de estados y funciones

--- my-react-app-with-ts/src/App.tsx
+++ my-react-app-with-ts/src/App.tsx
@@ -1,10 +1,20 @@
 import Button from './components/Button'
+import Counter from './components/Counter'
 import './App.css'
+
+function greet(name: string): string {
+  return `Hello, ${name}!`;
+}
 
 function App() {
   const handleClick = () => alert("Ya estoy clickeado !")
+  console.log(greet("World"));
 
-
-  return <Button onclick={handleClick} label="Click me"/>
+  return (
+    <>
+      <Button onclick={handleClick} label="Click me" />
+      <Counter />
+    </>
+  )
 }
-  export default App
+export default App
